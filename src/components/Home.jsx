ede13d75b6ffd11dc57d2a90e705bf5e7206dc22
LordDanny <56984709+dannydlor@users.noreply.github.com>
2021-10-19 17:44:06 -0500

    Diseño de Login y Registro

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,53 @@ import '.././App.css';
 function Home(){
     return (
 	<>
-		<center>
-			<div className="jumbotron">
-				<h1 className="display-4">¡Crea tu propia ENCUESTA!</h1>
-				
-				<p className="parrafo" > 
-				Fácil, potente, e innovador. Crea tus encuestas con facilidad y confianza! Herramientas estadísticas. Sistema innovador. 100% Gratis por siempre. Sencillo, potente, rápido
-				</p>
-				
-				<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<hr className="my-4" />
-				<p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-				<a className="btn btn-primary btn-lg" href="#home" role="button">Learn more</a>
+		<div className="grid">
+
+			<div className="grid-uno">
+				<div className="grid-anidado-uno">
+					<h1>Registrate <span>y Opina </span> contestando encuestas</h1>
+				</div>
+				<div className="grid-anidado-dos">
+					<img src="./mujer-encuesta.png" alt="" />
+				</div>
 			</div>
-		</center>
+			<div className="grid-dos">
+				<div className="container-grid-dos border m-4 position-relative">
+					<div className="container-formulario position-absolute top-50 start-50 translate-middle">
+						<p> ¡Regístrate YA!</p>
+						<form>
+							<div className="form-group">
+								<label for="exampleInputName">¿Cuál es tu primer nombre?</label>
+								<input type="name" className="form-control" id="exampleInputEmail1" aria-describedby="NameHelp" placeholder="Enter first name"/>
+							</div>
+							<div className="form-group">
+								<label for="exampleInputLastName">¿Cuál es tu primer apellido?</label>
+								<input type="lastname" className="form-control" id="exampleInputLastName" placeholder="Enter first lastname"/>
+							</div>
+							<div className="form-check">
+								<input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+								<label className="form-check-label" for="exampleCheck1">Check me out</label>
+							</div>
+								<button type="submit" className="btn btn-primary">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptas, distinctio provident aliquid magnam obcaecati commodi quam ipsa odit quasi? Debitis voluptatum, dolores vero recusandae assumenda adipisci nisi error ab.</p>
+			</div>
+			<div>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptas, distinctio provident aliquid magnam obcaecati commodi quam ipsa odit quasi? Debitis voluptatum, dolores vero recusandae assumenda adipisci nisi error ab.</p>
+			</div>
+			<div>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptas, distinctio provident aliquid magnam obcaecati commodi quam ipsa odit quasi? Debitis voluptatum, dolores vero recusandae assumenda adipisci nisi error ab.</p>
+			</div>
+			<div>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptas, distinctio provident aliquid magnam obcaecati commodi quam ipsa odit quasi? Debitis voluptatum, dolores vero recusandae assumenda adipisci nisi error ab.</p>
+			</div>
+
+		</div>
+		
 	</>
     )
 }
